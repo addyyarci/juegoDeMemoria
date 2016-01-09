e5c32171b82359d60a5d730064b5db2665b527e2
Maver <maver@Mac-Pro-de-Maver.local>
2016-01-09 00:30:46 -0600

    PreVersion

--- juegoDeMemoria.playground/section-1.swift
+++ juegoDeMemoria.playground/section-1.swift
@@ -2,4 +2,30 @@
 
 import UIKit
 
-var str = "Hello, playground"
+
+for var i = 0; i<=100; i++
+{
+    var rest = (i%5)
+    var par = (i%2)
+    
+    //el numero es divisible entre 5
+    if rest == 0 {
+        print("# \(i) + ''Bingo!!!''\n")
+    }
+    
+    //el numero es par
+    if par == 0{
+        print("# \(i) + ''par!!!''\n")
+    }
+        //condicion impar
+    else{
+        print("# \(i) + ''impar!!!''\n")
+    }
+    
+    //rango de 30 a 40
+    if i >= 30 && i <= 40
+    {
+        print("# \(i) + ''Viva Swift!!!''\n")
+    }
+    
+}
